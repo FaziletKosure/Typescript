9e2a1a9bc279ccb1f6a4c567e26350e9193c4440
FaziletKosure <faziletkosure1@gmail.com>
2021-03-06 14:00:09 +0100

    added typescript.ts

---
+++ typescript.js
@@ -0,0 +1,7 @@
+// console.log("Hello world");
+var a;
+a = "Hello World ";
+var b;
+b = 22;
+console.log("a:", a, "b:", b);
+console.log('what is up');
---
+++ typescript.ts
@@ -0,0 +1,10 @@
+// console.log("Hello world");
+let a : string;
+a="Hello World "
+let b : number;
+
+b = 22
+console.log("a:",a,"b:",b);
+console.log('what is up');
+
+
